d23472242db69d4713e153881b0e88f34523a419
Bryan Hundven <bryanhundven@gmail.com>
2012-03-26 02:19:47 -0700

    Add the right nvram_net.txt path for bcm4329 and add watchdog and timeout arguments
    Change-Id: I51e8ae7a00d76da1a59b8d433560cc4dcd4ebf5a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -97,8 +97,8 @@ BOARD_WLAN_DEVICE := bcm4329
 WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/bcm4329.ko"
 WIFI_DRIVER_FW_STA_PATH     := "/vendor/firmware/fw_bcm4329.bin"
 WIFI_DRIVER_FW_AP_PATH      := "/vendor/firmware/fw_bcm4329_apsta.bin"
-WIFI_DRIVER_MODULE_NAME     :=  "bcm4329"
-WIFI_DRIVER_MODULE_ARG      :=  "firmware_path=/vendor/firmware/fw_bcm4329.bin nvram_path=/vendor/firmware/nvram_net.txt"
+WIFI_DRIVER_MODULE_NAME     := "bcm4329"
+WIFI_DRIVER_MODULE_ARG      := "firmware_path=/vendor/firmware/fw_bcm4329.bin nvram_path=/system/etc/wifi/nvram_net.txt dhd_watchdog_ms=10 dhd_poll=1"
 
 # Recovery
 BOARD_RECOVERY_HANDLES_MOUNT := true
