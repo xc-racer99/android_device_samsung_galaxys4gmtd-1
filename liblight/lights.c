4c49562c432729c0122da99fd28b61503dc200c2
Max Fierke <max@maxfierke.com>
2012-04-07 10:13:00 -0500

    Add BLN support to liblights Adapted from Michael Richer's BLN patches for i9000
    Change-Id: Iea66adb29fc9b86e471c4cb519a666ceff2fa838

--- liblight/lights.c
+++ liblight/lights.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2008 The Android Open Source Project
+ * Copyright (C) 2010 Michael Richter (alias neldar)
  * Copyright (C) 2011 <kang@insecure.ws>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -19,6 +20,7 @@
 #include <cutils/log.h>
 #include <stdint.h>
 #include <string.h>
+#include <unistd.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <pthread.h>
@@ -29,8 +31,33 @@
 static pthread_once_t g_init = PTHREAD_ONCE_INIT;
 static pthread_mutex_t g_lock = PTHREAD_MUTEX_INITIALIZER;
 
+static int g_haveTrackballLight = 0;
+static struct light_state_t g_notification;
+static struct light_state_t g_battery;
+static int g_backlight = 170;		//modified by mr
+static int g_trackball = -1;
+static int g_buttons = 0;
+static int g_attention = 0;
+
+#if 0
+/* samsung  does have this sys/file in their liblights, may be they
+*  implemented a specific light intensity/lcd-backlight brightness function
+*/
+
+char const*const LIGHTSENSOR_FILE
+        = "/sys/class/lightsensor/switch_cmd/lightsensor_file_state";
+#endif
+
+char const*const BUTTON_FILE
+        = "/sys/class/misc/melfas_touchkey/brightness";
+	
+char const*const NOTIFICATION_FILE
+        = "/sys/class/misc/backlightnotification/notification_led";
+	
+//end modified by mr
+
 char const *const LCD_FILE = "/sys/class/backlight/s5p_bl/brightness";
-char const *const LED_FILE = "/sys/class/misc/notification/led";
+//char const *const LED_FILE = "/sys/class/misc/notification/led";
 
 static int write_int(char const *path, int value)
 {
@@ -65,7 +92,32 @@ static int rgb_to_brightness(struct light_state_t const *state)
 		+ (150*((color>>8) & 0x00ff)) + (29*(color & 0x00ff))) >> 8;
 }
 
+static int set_light_buttons(struct light_device_t* dev,
+        	struct light_state_t const* state)
+{
+    int err = 0;
+    int on = is_lit(state);
+    pthread_mutex_lock(&g_lock);
+    g_buttons = on;
+    err = write_int(BUTTON_FILE, on?1:2);
+    pthread_mutex_unlock(&g_lock);
+    return err;
+}
+
 static int set_light_notifications(struct light_device_t* dev,
+        	struct light_state_t const* state)
+{
+    int err = 0;
+    int on = is_lit(state);
+    LOGV("%s color=%08x flashMode=%d flashOnMS=%d flashOffMS=%d\n", __func__,
+          state->color, state->flashMode, state->flashOnMS, state->flashOffMS);
+    pthread_mutex_lock(&g_lock);
+    g_buttons = on;
+    err = write_int(NOTIFICATION_FILE, on?1:0);
+    pthread_mutex_unlock(&g_lock);
+    return err;
+}
+/*static int set_light_notifications(struct light_device_t* dev,
 			struct light_state_t const* state)
 {
 	int brightness =  rgb_to_brightness(state);
@@ -83,7 +135,7 @@ static int set_light_notifications(struct light_device_t* dev,
 	ret = write_int(LED_FILE, v);
 	pthread_mutex_unlock(&g_lock);
 	return ret;
-}
+}*/
 
 static int set_light_backlight(struct light_device_t *dev,
 			struct light_state_t const *state)
@@ -117,6 +169,8 @@ static int open_lights(const struct hw_module_t *module, char const *name,
 
 	if (0 == strcmp(LIGHT_ID_BACKLIGHT, name))
 		set_light = set_light_backlight;
+	else if (0 == strcmp(LIGHT_ID_BUTTONS, name))
+        set_light = set_light_buttons;
 	else if (0 == strcmp(LIGHT_ID_NOTIFICATIONS, name))
 		set_light = set_light_notifications;
 	else
@@ -147,7 +201,7 @@ const struct hw_module_t HAL_MODULE_INFO_SYM = {
 	.version_major = 1,
 	.version_minor = 0,
 	.id = LIGHTS_HARDWARE_MODULE_ID,
-	.name = "lights Module",
-	.author = "Google, Inc.",
+	.name = "Samsung Galaxy S 4G Custom Lights Module w/ BLN",
+	.author = "Google, Inc., Michael Richer, Max Fierke",
 	.methods = &lights_module_methods,
 };
