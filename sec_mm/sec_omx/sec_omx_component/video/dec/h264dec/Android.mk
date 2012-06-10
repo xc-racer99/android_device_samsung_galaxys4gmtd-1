399f9534b509ddf448f98c87f18b57a537e7f4af
Max Fierke <max@maxfierke.com>
2012-06-09 19:59:34 -0500

    Updated OMX libraries from ICS aries-common/crespo
    * These libraries allow recording crash-free, but are not encoding the video correctly. All video is green. I'm beginning to think that this is more of an issue with libstagefrighthw and/or liboverlay.
    
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- sec_mm/sec_omx/sec_omx_component/video/dec/h264dec/Android.mk
+++ sec_mm/sec_omx/sec_omx_component/video/dec/h264dec/Android.mk
@@ -22,7 +22,7 @@ LOCAL_C_INCLUDES := $(SEC_OMX_INC)/khronos \
 	$(SEC_OMX_TOP)/sec_osal \
 	$(SEC_OMX_TOP)/sec_omx_core \
 	$(SEC_OMX_COMPONENT)/common \
-	$(SEC_OMX_COMPONENT)/video/dec \
+	$(SEC_OMX_COMPONENT)/video/dec
 
 LOCAL_C_INCLUDES += $(SEC_OMX_TOP)/sec_codecs/video/mfc_c110/include
 
