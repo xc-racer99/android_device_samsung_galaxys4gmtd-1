a236e9431e8bb9bfac5fbc3b5b4cb56e04f3daf6
Bryan Hundven <bryanhundven@gmail.com>
2012-07-01 03:34:27 -0700

    Add TeamAcid versioning, goo.im support. Release 0.3!
    Change-Id: I8c28aaa2bb8eaeadd09c6bb1def1fb48e54e23f4

---
+++ version.mk
@@ -0,0 +1,9 @@
+TA_STABLE := 0
+TA_RELEASE := 3
+TA_VERSION := $(TA_STABLE).$(TA_RELEASE)
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.goo.developerid=TeamAcid
+	ro.goo.rom=CM9-galaxys4gmtd
+	ro.goo.version=$(TA_VERSION)
+	ro.goo.board=$(PRODUCT_DEVICE)
