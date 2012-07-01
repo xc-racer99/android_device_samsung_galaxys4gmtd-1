9b8244571fa3d2faff57ed56b204039fc6d5f3ff
Max Fierke <max@maxfierke.com>
2012-07-01 14:11:47 -0500

    Remove extra backslash
    Reported-by: Fernando Barillas <fbis251@gmail.com>

--- version.mk
+++ version.mk
@@ -6,4 +6,4 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ro.goo.developerid=TeamAcid \
 	ro.goo.rom=CM9-galaxys4gmtd \
 	ro.goo.version=$(TA_VERSION) \
-	ro.goo.board=$(PRODUCT_DEVICE) \
+	ro.goo.board=$(PRODUCT_DEVICE)
