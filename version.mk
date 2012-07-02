8701a7f09d29311ce7dfb8018e7fd8429ab9f3bc
Bryan Hundven <bryanhundven@gmail.com>
2012-07-02 14:31:52 -0700

    goo version must be an interger

--- version.mk
+++ version.mk
@@ -1,9 +1,7 @@
-TA_STABLE := 0
 TA_RELEASE := 3
-TA_VERSION := $(TA_STABLE).$(TA_RELEASE)
 
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.goo.developerid=TeamAcid \
 	ro.goo.rom=CM9-galaxys4gmtd \
-	ro.goo.version=$(TA_VERSION) \
+	ro.goo.version=$(TA_RELEASE) \
 	ro.goo.board=$(PRODUCT_DEVICE)
