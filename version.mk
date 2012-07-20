c1c4ee25ebc211be17f89589d824cc3e0f682a6a
Bryan Hundven <bryanhundven@gmail.com>
2012-07-19 20:05:21 -0700

    Remove ro.goo.board

--- version.mk
+++ version.mk
@@ -3,5 +3,4 @@ TA_RELEASE := 4
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.goo.developerid=TeamAcid \
 	ro.goo.rom=CM9-galaxys4gmtd \
-	ro.goo.version=$(TA_RELEASE) \
-	ro.goo.board=$(PRODUCT_DEVICE)
+	ro.goo.version=$(TA_RELEASE)
