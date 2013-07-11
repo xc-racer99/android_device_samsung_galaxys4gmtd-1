5aa01caa26b29adc9144741a1dfe9650765ba8cc
noiphonehere <noiphonehere@gmail.com>
2013-07-11 19:08:55 -0400

    Remove libGPS. We shouldn't need it.

--- device.mk
+++ device.mk
@@ -28,10 +28,6 @@ PRODUCT_COPY_FILES += \
 	device/samsung/vibrantmtd/keys/aries-keypad.kl:system/usr/keylayout/aries-keypad.kl \
 	device/samsung/vibrantmtd/keys/cypress-touchkey.kl:system/usr/keylayout/cypress-touchkey.kl
 
-# GPS
-PRODUCT_PACKAGES += \
-    gps.aries
-
 # Inherit common configuration
 $(call inherit-product, device/samsung/aries-common/device_base.mk)
 
