9043d8bffab0401ea81bafd10cc42ec9cfca2e67
noiphonehere <noiphonehere@gmail.com>
2013-07-11 19:32:51 -0400

    We should copy init.aries.rc, not init.target.rc.

--- device.mk
+++ device.mk
@@ -20,7 +20,7 @@ PRODUCT_COPY_FILES := \
 
 # Init files
 PRODUCT_COPY_FILES += \
-	device/samsung/galaxys4gmtd/ramdisk/init.target.rc:root/init.target.rc \
+	device/samsung/galaxys4gmtd/ramdisk/init.aries.rc:root/init.aries.rc \
 	device/samsung/galaxys4gmtd/ramdisk/init.aries.gps.rc:root/init.aries.gps.rc
 
 # Prebuilt kl keymaps
