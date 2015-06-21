4b8343faf4717ecf995fc1927448e5d2ee2c14ed
xc-racer99 <xc-racer2@live.ca>
2015-06-20 21:36:35 -0700

    Update BoardConfig.mk based on noiphonehere's work

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,9 +19,6 @@
 # Product-specific compile-time definitions.
 #
 
-# Telephony
-BOARD_USES_FROYO_RILCLIENT := true
-
 # Kernel Config
 TARGET_KERNEL_CONFIG := omni_galaxys4gmtd_defconfig
 
@@ -31,7 +28,30 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/galaxys4gmtd/bluet
 # Recovery
 BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/galaxys4gmtd/recovery/recovery_keys.c
 
-TARGET_OTA_ASSERT_DEVICE := galaxys4g,galaxys4gmtd,SGH-T959V
+TARGET_OTA_ASSERT_DEVICE := galaxys4g,galaxys4gmtd,SGH-T959V,SGH-T959W,SGH-T959P
 
 # Import the aries-common BoardConfigCommon.mk
 include device/samsung/aries-common/BoardConfigCommon.mk
+
+# Override stuff that doesn't match aries-common
+#TARGET_USERIMAGES_USE_EXT4 := false
+BOARD_SECOND_CAMERA_DEVICE := /dev/video2
+TARGET_PROVIDES_LIBCAMERA := true
+
+# Based on kernel header
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 403701760
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 545259520
+
+# TWRP options
+DEVICE_RESOLUTION := 480x800
+TW_NO_REBOOT_BOOTLOADER := true
+TW_FLASH_FROM_STORAGE := true
+# Leave this as false for now, we can attempt to enable it later when we
+# make sure that the ramdisk can be injected to
+TW_INCLUDE_INJECTTWRP := false
+# Override some settings in aries-common as we don't have an external_sd/emmc
+undefine TW_INTERNAL_STORAGE_PATH
+undefine TW_INTERNAL_STORAGE_MOUNT_POINT
+TW_EXTERNAL_STORAGE_PATH := "/sdcard"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "sdcard"
+TW_DEFAULT_EXTERNAL_STORAGE := true
