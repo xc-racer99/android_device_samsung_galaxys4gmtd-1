d20cc06a3eb77386ffac92ab05d0cb6a27b21db6
xc-racer99 <xc-racer2@live.ca>
2015-10-31 08:28:36 -0700

    Revert "Allow installation of gapps and SuperSu when updating from BML"
    Think its causing a brick
    
    This reverts commit baa067a996474404af5fd83f962bb8d9e6f8d549.

--- updater.sh
+++ updater.sh
@@ -172,9 +172,6 @@ elif /tmp/busybox test -e /dev/block/mtdblock0 && /tmp/busybox test -e /dev/bloc
     # unmount and format datadata
     /tmp/busybox umount -l /datadata
     /tmp/erase_image datadata
- 
-    # restart into recovery so the user can install further packages such as gapps and SuperSU before booting
-    /tmp/busybox touch /cache/.startrecovery
 
     # restore efs backup
     if /tmp/busybox test -e /sdcard/backup/efs.tar ; then
