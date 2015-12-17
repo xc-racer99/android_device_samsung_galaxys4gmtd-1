92e13329c284862e1d57669b6fd33238cb16d96a
xc-racer99 <xc-racer2@live.ca>
2015-12-17 14:54:18 -0800

    galaxys4gmtd: Sepolicy updates
    Comment out sepolicy's that contract neverallow rules
    Remove everythhing that's doubled with aries-common and only add in things that it doesn't have

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,9 +39,7 @@ TARGET_PROVIDES_LIBCAMERA := true
 TARGET_RECOVERY_FSTAB := device/samsung/galaxys4gmtd/fstab.aries
 
 # SELinux
-BOARD_SEPOLICY_DIRS := $(filter-out device/samsung/aries-common/sepolicy,$BOARD_SEPOLICY_DIRS)
-BOARD_SEPOLICY_DIRS += \
-	device/samsung/galaxys4gmtd/sepolicy
+BOARD_SEPOLICY_DIRS += device/samsung/galaxys4gmtd/sepolicy
 
 # Based on kernel header
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 508559360
