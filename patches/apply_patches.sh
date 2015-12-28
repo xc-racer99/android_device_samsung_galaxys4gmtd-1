f463de68cf4104310e4440d51c7d6e163d8e7570
xc-racer99 <xc-racer2@live.ca>
2015-12-28 14:09:32 -0800

    galaxys4gmtd: Add script to easily apply patches
    Easier than having to manually do each one.

---
+++ patches/apply_patches.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# Shell script to apply patches necessary to build for Galaxy S 4G devices
+
+pushd $(dirname "${0}") > /dev/null
+SCRIPTPATH=$(pwd -L)
+# Use "pwd -P" for the path without links. man bash for more info.
+popd > /dev/null
+
+SCRIPTPATH=$SCRIPTPATH"/"
+
+# Location of ANDROID_ROOT compared with $SCRIPTPATH
+ROOT_LOCATION=../../../../
+
+# PATCHFILE=name of patch to apply
+# DIRECTORY=directory to apply patch to relative to ANDROID_ROOT
+
+PATCHFILE[0]="android_art.patch"
+DIRECTORY[0]="art"
+
+PATCHFILE[1]="android_bionic.patch"
+DIRECTORY[1]="bionic"
+
+PATCHFILE[2]="android_bootable_recovery.patch"
+DIRECTORY[2]="bootable/recovery"
+
+PATCHFILE[3]="android_build.patch"
+DIRECTORY[3]="build"
+
+PATCHFILE[4]="android_external_busybox.patch"
+DIRECTORY[4]="external/busybox"
+
+PATCHFILE[5]="android_frameworks_base.patch"
+DIRECTORY[5]="frameworks/base"
+
+PATCHFILE[6]="android_frameworks_base_2.patch"
+DIRECTORY[6]="frameworks/base"
+
+PATCHFILE[7]="android_frameworks_native.patch"
+DIRECTORY[7]="frameworks/native"
+
+PATCHFILE[8]="android_system_bt.patch"
+DIRECTORY[8]="system/bt"
+
+PATCHFILE[9]="android_system_extras.patch"
+DIRECTORY[9]="system/extras"
+
+
+ARRAY_LENGTH=${#PATCHFILE[@]}
+COUNTER=0
+while [  $COUNTER -lt $ARRAY_LENGTH ]; do
+        cd $SCRIPTPATH$ROOT_LOCATION${DIRECTORY[$COUNTER]} || exit
+	ABS_PATCHFILE=$SCRIPTPATH${PATCHFILE[$COUNTER]}
+
+	CMD_OUTPUT=$(git am $ABS_PATCHFILE)
+
+	if [[ $CMD_OUTPUT =~ error.|fail. ]]; then
+		git am --abort
+		echo Ran git am --abort
+	fi
+
+	let COUNTER=COUNTER+1
+done
