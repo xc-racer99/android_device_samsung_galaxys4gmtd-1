06f9a77ea3eb4bc6dbd8f1d8a9670799a1e1a0f0
xc-racer99 <xc-racer2@live.ca>
2016-04-12 19:36:50 -0700

    telusgalaxys4gmtd: Add prebuilt kernel
    Essentially, this is just the zImage as our zImage==boot.img==kernel.
    
    See usr/README in kernel/samsung/aries on how to create it.

--- device.mk
+++ device.mk
@@ -79,6 +79,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Inherit Aries common device configuration.
 $(call inherit-product, device/samsung/aries-common/device_base.mk)
 
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+LOCAL_KERNEL := device/samsung/telusgalaxys4gmtd/zImage
+else
+LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
+
 # See comment at the top of this file. This is where the other
 # half of the device-specific product definition file takes care
 # of the aspects that require proprietary drivers that aren't
