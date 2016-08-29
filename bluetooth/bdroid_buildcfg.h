fdb69eaec43b5635454ee1b467ae21195c34adf5
xc-racer99 <xc-racer2@live.ca>
2016-08-28 19:03:44 -0700

    galaxys4gmtd: Add a new bluetooth flag
    Our kernel is very old!

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -23,4 +23,6 @@
 #define BTA_GATT_INCLUDED FALSE
 #define SMP_INCLUDED FALSE
 
+/* Defined if the kernel does not have support for CLOCK_BOOTTIME_ALARM */
+#define KERNEL_MISSING_CLOCK_BOOTTIME_ALARM TRUE
 #endif
